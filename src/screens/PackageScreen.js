6a0b0810e52539f3704aa447031f26ef2408a677
Pasandi Ranganath <2022cs162@stu.ucsc.cmb.ac.lk>
2024-05-22 21:07:42 +0530

    Add package screen to src/screen

--- src/screens/PackageScreen.js
+++ src/screens/PackageScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import BottomTabNavigator from './components/BottomTabNavigator';
+import BottomTabNavigator from '../../components/BottomTabNavigator';
 
 export default function App() {
   return (
